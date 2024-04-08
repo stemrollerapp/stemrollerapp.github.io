364e222fa37fb8bdd47e860a68ddf830521cd078
iffyloop <35245220+iffyloop@users.noreply.github.com>
2024-04-08 03:08:59 -0500

    Update download links

--- src/utils/downloadLinks.js
+++ src/utils/downloadLinks.js
@@ -1,8 +1,8 @@
 import { readable } from 'svelte/store'
 
 export const downloadUrls = {
-  win: 'https://github.com/stemrollerapp/stemroller/releases/download/2.0.3/stemroller-2.0.3-win-cuda.exe',
-  mac: 'https://github.com/stemrollerapp/stemroller/releases/download/2.0.3/stemroller-2.0.3-mac.zip',
+  win: 'https://github.com/stemrollerapp/stemroller/releases/download/2.0.4/stemroller-2.0.4-win-cuda.exe',
+  mac: 'https://github.com/stemrollerapp/stemroller/releases/download/2.0.4/stemroller-2.0.4-mac.zip',
 }
 
 export const defaultDownloadOs = readable(null, (set) => {
